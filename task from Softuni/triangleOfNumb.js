fde4f9114e7c7d8b19423dab8025eb5289bcc99d
Hristo Chilov <103407076+Supevo@users.noreply.github.com>
2023-01-16 19:49:01 +0200

    Create triangleOfNumb.js

---
+++ task from Softuni/triangleOfNumb.js
@@ -0,0 +1,10 @@
+function triangleOfNumb(number) {
+    let print = "";
+
+    for (let i = 1; i <= number; i++) {
+        let saveNumb = `${i} `;
+
+        print = `${saveNumb.repeat(i)}`;
+        console.log(`${print.trimEnd()}`);
+    }
+}
